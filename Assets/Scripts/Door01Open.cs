856d041e74e1c2b98a9e49c01c69d99e308f72ee
GideonUng <gidcheen@gmail.com>
2015-01-08 03:07:44 +0100

    Added Border Level office

---
+++ Assets/Scripts/Door01Open.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door01Open : MonoBehaviour 
+{
+	public Animator anim;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			anim.SetBool("Open", true);
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			anim.SetBool("Open", false);
+		}
+	}
+}
