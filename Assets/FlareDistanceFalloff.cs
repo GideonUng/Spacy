ebfd6785f2cf3e836aa7e7599d13a40993bd6ce5
GideonUng <gidcheen@gmail.com>
2015-02-02 21:19:58 +0100

    added flare distance falloff

---
+++ Assets/FlareDistanceFalloff.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlareDistanceFalloff : MonoBehaviour
+{
+
+    private GameObject mainCamera;
+    private LensFlare flare;
+
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        flare = GetComponent<LensFlare>();
+    }
+
+    void LateUpdate()
+    {
+        flare.brightness = 100 - (transform.position - mainCamera.transform.position).magnitude;
+    }
+}
